58f265d643ad61f710f9d64bf33328176702e22d
johannmuenchhagen <johann.muenchhagen@gmail.com>
2023-02-07 13:46:50 +0100

    den ConnectionManager separiert

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from typing import List
 from fastapi.responses import HTMLResponse
+from connectionManager import ConnectionManager
 
 app = FastAPI()
 html = """
@@ -39,26 +39,6 @@ html = """
     </body>
 </html>
 """
-
-class ConnectionManager:
-
-    def __init__(self):
-        self.active_connections: List[WebSocket] = []
-
-    async def connect(self, websocket: WebSocket):
-        await websocket.accept()
-        self.active_connections.append(websocket)
-
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
-
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_json(message)
-
-    async def broadcast(self, message: str):
-        for connection in self.active_connections:
-            await connection.send_json(message)
-
 
 manager = ConnectionManager()
 
