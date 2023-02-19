0c30a8e03787b4154080cfab1a5f7e19c468c818
Lucas Schmitz <mrlucas.schmitz@aol.de>
2023-02-19 22:09:04 +0100

    refactor(frontend)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,91 +1,31 @@
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <CardGreeting msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <v-app>
+    <v-app-bar :elevation="2" density="compact" color="tertiary">
+      <template v-slot:prepend>
+        <v-icon icon="mdi-ferry"></v-icon>
+      </template>
+      <v-app-bar-title>Schiffe versenken</v-app-bar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon icon="mdi-magnify"></v-icon>
+      </v-btn>
+      <v-btn
+        icon
+        href="https://github.com/JohannMuenchhagen/Schiffe-versenken"
+        target="_blank"
+      >
+        <v-icon icon="mdi-github"></v-icon>
+      </v-btn>
+      <SettingsDialog></SettingsDialog>
+    </v-app-bar>
+    <v-main>
+      <RouterView />
+    </v-main>
+  </v-app>
 </template>
 
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-import CardGreeting from "./components/CardGreeting.vue";
+import SettingsDialog from "@/components/SettingsDialog.vue";
 </script>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
