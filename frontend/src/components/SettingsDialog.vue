232a686e136d694ba7e69b9be1dc14fe244a7a9b
Lucas Schmitz <mrlucas.schmitz@aol.de>
2023-02-20 10:09:01 +0100

    feat(themeChanger): add theme changer

--- frontend/src/components/SettingsDialog.vue
+++ frontend/src/components/SettingsDialog.vue
@@ -5,12 +5,26 @@
   <v-dialog v-model="dialog" width="auto" transition="dialog-bottom-transition">
     <v-card title="Einstellungen" class="dialogWrapper">
       <v-card-text>
-        <v-switch color="secondary" label="Darkmode einschalten"></v-switch>
+        <v-switch
+          v-model="themeTmp"
+          true-value="dark"
+          false-value="light"
+          color="secondary"
+          label="Darkmode einschalten"
+          @click="toggelTheme()"
+        ></v-switch>
       </v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
         <v-btn color="tertiary" @click="dialog = false">Schließen</v-btn>
-        <v-btn color="secondary" @click="dialog = false">Speichern</v-btn>
+        <v-btn
+          color="secondary"
+          @click="
+            save();
+            dialog = false;
+          "
+          >Speichern</v-btn
+        >
       </v-card-actions>
     </v-card>
   </v-dialog>
@@ -18,8 +32,24 @@
 
 <script setup lang="ts">
 import { ref } from "vue";
+import { useTheme } from "vuetify";
 
 let dialog = ref<boolean>(false);
+
+const theme = useTheme();
+let themeTmp = ref(theme.global.name.value);
+
+function toggelTheme() {
+  if (themeTmp.value === "dark") {
+    themeTmp.value = "light";
+  } else {
+    themeTmp.value = "dark";
+  }
+}
+
+function save() {
+  theme.global.name.value = themeTmp.value;
+}
 </script>
 
 <style scoped></style>
