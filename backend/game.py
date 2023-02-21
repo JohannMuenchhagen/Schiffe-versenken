4362122e4d2ce14092f9e30cc5d14809e018a10b
Johann Muenchhagen <johann.muenchhagen@gmail.com>
2023-02-21 12:56:29 +0100

    remove print statement

--- backend/game.py
+++ backend/game.py
@@ -7,8 +7,8 @@ class Game:
         self.game_id = game_id
         self.player1 = Player()
         self.player2 = Player()
-        self.player1_ships = []
-        self.player2_ships = []
+        self.player1_ships = [[], [], [], []]
+        self.player2_ships = [[], [], [], []]
         self.player1_ships_set = {'Battleship': 0, 'Corvettes': 0, 'Destroyer': 0, 'Submarine': 0}
         self.player2_ships_set = {'Battleship': 0, 'Corvettes': 0, 'Destroyer': 0, 'Submarine': 0}
         self.player1_moves = []
@@ -42,8 +42,38 @@ class Game:
             else:
                 return True
 
-    def set_ships(self, player_id: int, ship_type: str, ship_start_pos: tuple, ship_end_pos: int) -> str:
+    def set_ships(self, player_id: int, ship_type: str, ship_start_pos: tuple, ship_end_pos: tuple) -> dict:
+        match ship_type:
+            case 'Battleship':
+                ship = Battleship(start_pos=ship_start_pos, end_pos=ship_end_pos)
+                index = 0
+                if not ship.check_length(ship.length):
+                    return {'Error': 'The ship length does not meet the specification of 5'}
+            case 'Corvettes':
+                ship = Corvettes(start_pos=ship_start_pos, end_pos=ship_end_pos)
+                index = 1
+                if not ship.check_length(ship.length):
+                    return {'Error': 'The ship length does not meet the specification of 4'}
+            case 'Destroyer':
+                ship = Destroyer(start_pos=ship_start_pos, end_pos=ship_end_pos)
+                index = 2
+                if not ship.check_length(ship.length):
+                    return {'Error': 'The ship length does not meet the specification of 3'}
+            case 'Submarine':
+                ship = Submarine(start_pos=ship_start_pos, end_pos=ship_end_pos)
+                index = 3
+                if not ship.check_length(ship.length):
+                    return {'Error': 'The ship length does not meet the specification of 2'}
+            case _:
+                return {'Error': 'No ship provided'}
+
         if player_id == 1:
-            pass
+            self.player1_ships_set[ship_type] += 1
+            self.player1_ships[index].append(ship.get_positions())
+            return {'Message': f'Ship: {ship_type} successfully placed'}
+        elif player_id == 2:
+            self.player2_ships_set[ship_type] += 1
+            self.player2_ships[index].append(ship.get_positions())
+            return {'Message': f'Ship: {ship_type} successfully placed'}
         else:
-            pass
+            return {'Error': 'Wrong player id'}
