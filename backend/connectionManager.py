d3b436ba6438ea635ee915d10111e9558b2f930d
Johann Muenchhagen <johann.muenchhagen@gmail.com>
2023-02-24 12:38:26 +0100

    add first variable to connectionManager.py

--- backend/connectionManager.py
+++ backend/connectionManager.py
@@ -7,8 +7,8 @@ class ConnectionManager:
 
     def __init__(self):
         self.active_connections: List[WebSocket] = []
-        self.active_games: int = 0
-        self.active_player: int = 0
+        self.active_games: int = 0  # counter of active player
+        self.active_player: int = 0  # counter of active games
 
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
@@ -17,7 +17,7 @@ class ConnectionManager:
     def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
         self.active_player -= 1
-        if self.active_player % 2 == 0:
+        if self.active_player % 2 == 0:  # if the number of players is equal, after 1 is leaving, remove 1 game
             self.active_games -= 1
 
     async def send_personal_message(self, message: str, websocket: WebSocket):
