2431216a1e60307ed787982973c7296d6353e1b5
Johann Muenchhagen <johann.muenchhagen@gmail.com>
2023-02-28 08:53:42 +0100

    add join and set

--- backend/connectionManager.py
+++ backend/connectionManager.py
@@ -1,5 +1,8 @@
 from typing import List
 from fastapi import WebSocket
+import json
+
+from backend.game import Game
 
 
 # TODO refactor ConnectionManager
@@ -9,6 +12,7 @@ class ConnectionManager:
         self.active_connections: List[WebSocket] = []
         self.active_games: int = 0  # counter of active player
         self.active_player: int = 0  # counter of active games
+        self.current_games = {}  # active dict of active games
 
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
@@ -26,3 +30,31 @@ class ConnectionManager:
     async def broadcast(self, message: str):
         for connection in self.active_connections:
             await connection.send_json(message)
+
+    def check_type(self, data: json, websocket: WebSocket):
+        data: dict = json.loads(data)
+        match data['Type']:
+            case 'initialize Game':
+                return self.initialize_game(websocket)
+            case 'Join':
+                if data['GameID'] not in self.current_games:
+                    return json.dumps({'Error': 'Unable to Join', 'Description': 'Game doesnt exist'})
+                return self.join(data, websocket)
+
+    def initialize_game(self, websocket: WebSocket):
+        self.active_games += 1
+        self.active_player += 1
+        game = Game(self.active_games)
+        self.current_games[self.active_games] = game
+        game.set_player(websocket)
+        return json.dumps({'Message': 'Game successfully initialized', 'GameID': game.game_id,
+                           'PlayerID': game.player1.playerID})
+
+    def join(self, data: dict, websocket: WebSocket):
+        game_id = data['GameID']
+        game = self.current_games[game_id]
+        msg = game.set_player(websocket)
+        if 'Error' in msg:
+            return json.dumps(msg)
+        self.active_player += 1
+        return json.dumps({'Message': 'Join successful', 'PlayerID': game.player2.playerID})
