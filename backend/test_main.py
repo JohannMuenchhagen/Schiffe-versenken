31d1dd16ad07ee783304902b6442b8630cce9d88
Johann Muenchhagen <johann.muenchhagen@gmail.com>
2023-03-03 17:45:57 +0100

    deletet

--- backend/test_main.py
+++
@@ -1,7 +0,0 @@
-from fastapi.testclient import TestClient
-from main import app
-
-def test_read_main():
-    client = TestClient(app)
-    response = client.get("/")
-    assert response.json() == {'message': 'Hello User'}
