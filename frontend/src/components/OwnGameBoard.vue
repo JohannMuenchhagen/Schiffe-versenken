e0bcc3fb46ce34b3c2ace64e7703ef89ab5ec95f
Katarina <minin.21.11@gmail.com>
2023-03-15 09:00:06 +0100

    correc isTakenbaanship and tangent

--- frontend/src/components/OwnGameBoard.vue
+++ frontend/src/components/OwnGameBoard.vue
@@ -33,6 +33,9 @@ let endPosition: undefined | { x: number; y: number } = undefined;
 let lengthDeletedShipX = 0;
 let lengthDeletedShipY = 0;
 
+let lengthSelectedShipX = 0;
+let lengthSelectedShipY = 0;
+
 let remaining5LengthShip = 1;
 let remaining4LengthShip = 2;
 let remaining3LengthShip = 3;
@@ -60,30 +63,33 @@ function placeShip(event: any, x: number, y: number) {   //vom Platzieren prüfe
       return;
     }
 
-    if (isTakenByAnotherShip(x, y)) {
-      snackbarStore.callSnackbar("Die Schiffe dürfen nicht über Eck gebaut sein oder Ausbuchtungen besitzen!");
-      return;
-    }
-
     selectedShipDirectionHorizontal = shipStore.getDirechtionsForShips[selectedShipLength - 2];
+    let xEnd = x;
+    let yEnd = y;
 
     if (isCrossedBorder(x, y, selectedShipDirectionHorizontal)) {
       snackbarStore.callSnackbar("Das Schiff überschreitet eine Grenze!");
       return;
     }
 
-    let xEnd = x;
-    let yEnd = y;
-
-    
     if(selectedShipDirectionHorizontal) {
       xEnd = x + selectedShipLength! - 1;
+      if (isTakenByAnotherShip(x, y, xEnd, yEnd)) {
+      snackbarStore.callSnackbar("Dort befindet sich bereits ein Schiff!");
+      return;
+      }
       if (isTangentToAnotherShip(x, y, xEnd, yEnd)) {
+        snackbarStore.callSnackbar("Die Schiffe dürfen nicht über Eck gebaut sein oder Ausbuchtungen besitzen!");
         return;
       }
     } else {
       yEnd = y + selectedShipLength! - 1;
+      if (isTakenByAnotherShip(x, y, xEnd, yEnd)) {
+      snackbarStore.callSnackbar("Dort befindet sich bereits ein Schiff!");
+      return;
+      }
       if (isTangentToAnotherShip(x, y, xEnd, yEnd)) {
+        snackbarStore.callSnackbar("Die Schiffe dürfen nicht über Eck gebaut sein oder Ausbuchtungen besitzen!");
         return;
       }
     }
@@ -168,48 +174,28 @@ function isCrossedBorder(x: number, y: number, directionHorizontal: boolean): bo
   return false;
 }
 
-function isTangentToAnotherShip(xStart: number, yStart: number, xEnd: number, yEnd: number): boolean {  //prüft, ob es eine Ecke ist
-
-for (let i = xStart - 2; i <= xEnd; i++) {
-  for (let j = yStart - 2; j <= yEnd; j++){    
+function isTangentToAnotherShip(xStart: number, yStart: number, xEnd: number, yEnd: number): boolean { 
+for (let i = xStart - 1; i <= xEnd + 1; i++) {
+  for (let j = yStart - 1; j <= yEnd + 1; j++){    
           if (i < 0 || j < 0) { continue; } 
-          if (document
-          .getElementById("myBoard")
-          ?.getElementsByClassName("v-row")
-          [j]?.getElementsByClassName("v-col")
-          [i]?.firstElementChild?.firstElementChild?.classList.contains(
-            "mdi-ferry"
-          ) === true) 
-          {
-            return true;
-          }
+          if(shipStore.getPlacedShips.find((value) => (i >= value.startPos.x && (value.endPos.x >= i)) 
+                                                 && ((j >= value.startPos.y) && (value.endPos.y >= j))) != undefined){
+                                                            return true;
+                                                          }
     }
   }
 return false;
 }
 
-function isTakenByAnotherShip(x: number, y: number): boolean {  //prüft, ob ein weiteren Schiff da ist
-  for (let i = x - 1; i < selectedShipLength! + x - 1; i++) {
-    for (let j = y - 1; j < selectedShipLength! + y - 1; j++){
-      if ((document
-            .getElementById("myBoard")
-            ?.getElementsByClassName("v-row")
-            [y - 1]?.getElementsByClassName("v-col")
-            [i]?.firstElementChild?.firstElementChild?.classList.contains(
-              "mdi-ferry"
-            ) === true) 
-            || (document
-                .getElementById("myBoard")
-                ?.getElementsByClassName("v-row")
-                [j]?.getElementsByClassName("v-col")
-                [x-1]?.firstElementChild?.firstElementChild?.classList.contains(
-                  "mdi-ferry"
-                ) === true)
-                )
-                {
-                  snackbarStore.callSnackbar("Dort befindet sich bereits ein Schiff!");
-                  return true;
-                }
+function isTakenByAnotherShip(xStart: number, yStart: number, xEnd: number, yEnd: number): boolean {  //prüft, ob ein weiterer Schiff da ist
+  
+  for (let i = xStart; i <= xEnd; i++) {
+    for (let j = yStart; j <= yEnd; j++){
+      if (i < 0 || j < 0) { continue; } 
+          if(shipStore.getPlacedShips.find((value) => (i >= value.startPos.x && (value.endPos.x >= i)) 
+                                                 && ((j >= value.startPos.y) && (value.endPos.y >= j))) != undefined){
+                                                            return true;
+                                                          }
       }
     }
   return false;
